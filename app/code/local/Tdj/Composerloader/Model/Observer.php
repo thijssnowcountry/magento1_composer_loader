a0474dc252753ec23161d447dc02bbd7b44d95a0
Thijs de Jong <thijs@snowcountry.nl>
2017-03-24 20:28:03 +0100

    Code reformat

--- app/code/local/Tdj/Composerloader/Model/Observer.php
+++ app/code/local/Tdj/Composerloader/Model/Observer.php
@@ -1,23 +1,21 @@
 <?php
-   /**
-     * Inspired by Dave Nacaulay http://davemacaulay.com/how-to-include-composer-libraries-within-magento/
-     */
+
+/**
+ * Inspired by Dave Nacaulay http://davemacaulay.com/how-to-include-composer-libraries-within-magento/
+ */
 class Tdj_Composerloader_Model_Observer
 {
 
-			public function controllerFrontInitBefore(Varien_Event_Observer $observer)
-			{
-				    self::init();
-			}
-		
-		/**
-     * Add in auto loader for Elasticsearch components
-     */
+    public function controllerFrontInitBefore(Varien_Event_Observer $observer)
+    {
+        self::init();
+    }
+
     static function init()
     {
-        // Add our vendor folder to our include path
+        // Add the composer vendor folder to our include path
         set_include_path(get_include_path() . PATH_SEPARATOR . Mage::getBaseDir('lib') . DS . 'Composer' . DS . 'vendor');
- 
+
         // Include the autoloader for composer
         require_once(Mage::getBaseDir('lib') . DS . 'Composer' . DS . 'vendor' . DS . 'autoload.php');
     }
